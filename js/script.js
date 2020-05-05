be5ca7321958353f715d143ea9b17da3fbe5b633
locngo3003 <locnxse04549@gmail.com>
2020-05-05 01:17:37 -0700

    Initial commit

---
+++ js/script.js
@@ -0,0 +1,13 @@
+function enlangeImage(e) {
+    e.style.height=200+"px"
+
+}
+function normalImage(e) {
+    e.style.height=128+"px"
+}
+function navCategories(e) {
+ switch (e.id) {
+     case "dog":
+
+ }
+}
